060a14c7c827ae69c843d941ba41f779a6504f69
Oliver Griffin <olivergriffin2991@gmail.com>
2024-07-04 18:52:47 +0000

    added random number generators to runGame()

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+/**
+ * The main game "loop", calledwhen the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
 
+    //creates two variables and assigns random numbers (between 1-25) to them
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 }
 
 function checkAnswer() {
